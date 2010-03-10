8e0b5bcc7abe513fd763b4a984120e1fc83f1e4b
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2010-03-10 17:35:39 +0100

    Updated MyextralifeComic to work by using archive-dates for its comic_ids.

--- lib/myextralife_comic.rb
+++ lib/myextralife_comic.rb
@@ -1,23 +1,25 @@
 require './lib/comic'
+require 'chronic'
 class MyextralifeComic < Comic
   BASE_URL = "http://www.myextralife.com"
   LATEST_URL = BASE_URL
   ARCHIVE_URL = "http://www.myextralife.com/the-archive/comics-by-year/"
-  def image_url(comic_id)
-    File.join(BASE_URL,'comics/cad',"#{comic_id}.jpg")
+  def image_urls(comic_id)
+    [
+      File.join(BASE_URL,'comics',"#{comic_id}.gif"),
+      File.join(BASE_URL,'comics',"#{comic_id}.jpg")
+    ]
   end
 
   def get_latest_id
     doc = Nokogiri::HTML(open(ARCHIVE_URL))
-    doc.css(".archive-title/a").first[:href].sub(File.join(BASE_URL,'comic/'),'').sub('/','')
+    doc.css(".archive-date").map{|a|Chronic.parse("#{a.text} #{Time.now.year}").strftime("%Y-%m-%d")}
   end
 
   def get_all_ids_since_last_saved
     episodes,year = get_episodes_from_archive
-    puts episodes.to_yaml
     until episodes.include?(last_saved_id)
       older_episodes,year = get_episodes_from_archive(year - 1)
-      puts older_episodes.to_yaml
       break if older_episodes == []
       episodes = older_episodes + episodes
     end
@@ -29,7 +31,7 @@ class MyextralifeComic < Comic
   def get_episodes_from_archive(year=nil)
     year ||= Time.now.year
     doc = Nokogiri::HTML(open(File.join(ARCHIVE_URL,"?archive_year=#{year}")))
-    episodes = doc.css(".archive-title/a").map{|a|a[:href].sub(File.join(BASE_URL,'comic/'),'').sub('/','')}
+    episodes = doc.css(".archive-date").map{|a|Chronic.parse("#{a.text} #{year}").strftime("%Y-%m-%d")}
     return episodes,year
   end
 end
